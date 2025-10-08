cfa40c0f1070429be7c5fffcd01cd9f7e42ef198
Saksham <sleeptillash@gmail.com>
2025-10-08 16:09:59 +0000

    starting with event and document

--- jsweb/scripts.js
+++ jsweb/scripts.js
@@ -1,2 +1,13 @@
-console.log("hello from script");
-console.log(document.firstChild);
+// example-1
+document.getElementById("changeTextButton").addEventListener("click", () => {
+  let para = document.getElementById("mypara");
+  para.textContent = " this is the changed text";
+});
+
+//example-2
+let clicker_1 = document.getElementById("sel2");
+
+clicker_1.addEventListener("click", () => {
+  const urlToOpen = "https://www.google.com";
+  window.open(urlToOpen, "_blank");
+});
