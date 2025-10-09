f3f602f12795c037553445680d7eede7f0b40f01
Saksham <sleeptillash@gmail.com>
2025-10-09 09:38:05 +0000

    working with DOM

--- jsweb/scripts.js
+++ jsweb/scripts.js
@@ -1,13 +1,37 @@
 // example-1
-document.getElementById("changeTextButton").addEventListener("click", () => {
-  let para = document.getElementById("mypara");
-  para.textContent = " this is the changed text";
-});
+document
+  .getElementById("changeTextButton")
+  .addEventListener("click", function () {
+    let para = document.getElementById("mypara");
+    para.textContent = " this is the changed text";
+  });
 
 //example-2
-let clicker_1 = document.getElementById("sel2");
 
-clicker_1.addEventListener("click", () => {
-  const urlToOpen = "https://www.google.com";
-  window.open(urlToOpen, "_blank");
+document.getElementById("HighlightCity").addEventListener("click", function () {
+  let citiesList = document.getElementById("cities");
+  citiesList.firstElementChild.classList.add("highlight");
 });
+
+//example-3
+
+document.getElementById("ChangeOrder").addEventListener("click", function () {
+  let CoffeeList = document.getElementById("coffeeType");
+  CoffeeList.textContent = "Espresso";
+  CoffeeList.style.backgroundColor = "Brown";
+});
+//example-4
+document.getElementById("addNewItem").addEventListener("click", function () {
+  let newitem = document.createElement("li");
+  newitem.textContent = "eggs";
+
+  let shoppingList = document.getElementById("shoppingList");
+  shoppingList.appendChild(newitem);
+});
+
+//example-5
+document
+  .getElementById("removeLastItem")
+  .addEventListener("click", function () {
+    document.getElementById("taskList").lastElementChild.remove();
+  });
